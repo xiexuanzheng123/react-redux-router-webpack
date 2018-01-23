bed19de2b786c91d591d4b290b05e0e314d90b80
xiexuanzheng123 <349328984@qq.com>
2018-01-23 16:52:30 +0800

    add students and modify the styles

--- src/models/students.js
+++ src/models/students.js
@@ -5,16 +5,32 @@ const students = [
         age: 24,
         sex: 'male',
         major: 'automation',
-        hobby: ['basketball', 'piano', 'game'],
-        profile: 'wait'
+        hobby: ['basketball', 'game'],
+        profile: 'Though I am just a virtual boy, I also want to bring warm to my frinds !!'
     },{
         id: '1',
         name: 'Sally',
         age: 22,
         sex: 'female',
-        major: 'automation',
-        hobby: ['basketball', 'piano', 'game'],
-        profile: 'wait'
+        major: 'Economic',
+        hobby: ['movie', ],
+        profile: 'I think John is a warm boy, I like him ^_^'
+    },{
+        id: '2',
+        name: 'Sedney',
+        age: 20,
+        sex: 'female',
+        major: 'English',
+        hobby: ['piano', 'music', 'game'],
+        profile: 'Shall we go to swimming ?'
+    },{
+        id: '3',
+        name: 'Jimmy',
+        age: 19,
+        sex: 'male',
+        major: 'Philosophy',
+        hobby: ['basketball', 'movie'],
+        profile: 'Emm... as we all know, this profile is fake'
     }
 ]
 export default students;
